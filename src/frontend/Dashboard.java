fdb04a9a541d13e0576a1cbf84cb2eab87a9544b
Akil Prasath R <akilprasathr@gmail.com>
2020-07-24 18:48:25 +0530

    Dashboard UI with buttons

--- src/frontend/Dashboard.java
+++ src/frontend/Dashboard.java
@@ -14,7 +14,11 @@ import java.sql.*;
 
 
 
-public class Dashboard extends JFrame{
+public class Dashboard extends JFrame implements ActionListener{
+	JButton manageFridge, manageItems, recipes,addFridge,organizeFridge,deleteFridge,createItem,deleteItem;
+	JPanel manageFridgePane,itemPanel,recipePanel;
+	JLabel fridgeHead,itemPanelhead;
+	Dimension centerPaneButtonDimension = new Dimension( 150,100 );
 	Dashboard(){
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		setSize(dim.width,dim.height);
@@ -30,24 +34,26 @@ public class Dashboard extends JFrame{
 		
 		navi.add(Box.createRigidArea(new Dimension(1,200)));
 		
-		JButton manageFridge = new JButton("Manage Fridge");
+		manageFridge = new JButton("Manage Fridge");
 		manageFridge.setAlignmentX(CENTER_ALIGNMENT);
 		manageFridge.setMaximumSize(new Dimension(200,100));
+		manageFridge.addActionListener(this);
 		navi.add(manageFridge);
 //		navi.add(Box.createVerticalGlue());
-		;
 //		navi.add(Box.createRigidArea(new Dimension(1,100)));
 		
-		JButton manageItems = new JButton("Manage Items");
+		manageItems = new JButton("Manage Items");
 		manageItems.setAlignmentX(CENTER_ALIGNMENT);
 		manageItems.setMaximumSize(new Dimension(200,100));
+		manageItems.addActionListener(this);
 		navi.add(manageItems);
 		
 //		navi.add(Box.createVerticalGlue());
 		
-		JButton recipes = new JButton("Recipes");
+		recipes = new JButton("Recipes");
 		recipes.setMaximumSize(new Dimension(200,100));
 		recipes.setAlignmentX(CENTER_ALIGNMENT);
+		recipes.addActionListener(this);
 		navi.add(recipes);
 		
 		navi.add(Box.createRigidArea(new Dimension(3,200)));
@@ -60,42 +66,117 @@ public class Dashboard extends JFrame{
 		topPane.add(title);
 		add(topPane,BorderLayout.NORTH);
 		
-		JPanel centerPane = new JPanel();
-		centerPane.setLayout(new BoxLayout(centerPane,BoxLayout.Y_AXIS));
-		centerPane.setPreferredSize(new Dimension((dim.width/4)*3,dim.height));
-		centerPane.setBackground(Color.cyan);
+		manageFridgePane = new JPanel();
+		manageFridgePane.setLayout(new BoxLayout(manageFridgePane,BoxLayout.Y_AXIS));
+		manageFridgePane.setPreferredSize(new Dimension((dim.width/4)*3,dim.height));
+		manageFridgePane.setBackground(Color.cyan);
+		manageFridgePane.setVisible(false);
 		
-		JLabel fridgeHead = new JLabel("Manage Fridges");
+		fridgeHead = new JLabel("Manage Fridges");
 		fridgeHead.setAlignmentX(CENTER_ALIGNMENT);
-		centerPane.add(fridgeHead);
+		manageFridgePane.add(fridgeHead);
 		
-		centerPane.add(Box.createRigidArea(new Dimension(0,100)));
+		manageFridgePane.add(Box.createRigidArea(new Dimension(0,100)));
 		
-		JButton addFridge = new JButton("Add Fridge");
-		addFridge.setMaximumSize(new Dimension( 150,100 ));
+		addFridge = new JButton("Add Fridge");
+		addFridge.setMaximumSize(centerPaneButtonDimension);
 		addFridge.setAlignmentX(CENTER_ALIGNMENT);
-		centerPane.add(addFridge);
+		manageFridgePane.add(addFridge);
 		
-		centerPane.add(Box.createRigidArea(new Dimension(0,100)));
+		manageFridgePane.add(Box.createRigidArea(new Dimension(0,100)));
 		
-		JButton organizeFridge = new JButton("Organize Fridge");
-		organizeFridge.setMaximumSize(new Dimension( 150,100 ));
+		organizeFridge = new JButton("Organize Fridge");
+		organizeFridge.setMaximumSize(centerPaneButtonDimension);
 		organizeFridge.setAlignmentX(CENTER_ALIGNMENT);
-		centerPane.add(organizeFridge);
+		manageFridgePane.add(organizeFridge);
 		
-		centerPane.add(Box.createRigidArea(new Dimension(0,100)));
+		manageFridgePane.add(Box.createRigidArea(new Dimension(0,100)));
 		
-		JButton deleteFridge = new JButton("Delete Fridge");
-		deleteFridge.setMaximumSize(new Dimension( 150,100 ));
+		deleteFridge = new JButton("Delete Fridge");
+		deleteFridge.setMaximumSize(centerPaneButtonDimension);
 		deleteFridge.setAlignmentX(CENTER_ALIGNMENT);
-		centerPane.add(deleteFridge);
+		manageFridgePane.add(deleteFridge);
+		
+		itemPanel = new JPanel();
+		itemPanel.setPreferredSize(new Dimension((dim.width/4)*3,dim.height));
+		itemPanel.setLayout(new BoxLayout(itemPanel,BoxLayout.Y_AXIS));
+		itemPanel.setVisible(false);
+		
+		itemPanelhead = new JLabel("Manage Items");
+		itemPanelhead.setAlignmentX(CENTER_ALIGNMENT);
+		itemPanel.add(itemPanelhead);
+		
+		itemPanel.add(Box.createRigidArea(new Dimension(0,100)));
+		
+		createItem = new JButton("Create Item");
+		createItem.setMaximumSize(centerPaneButtonDimension);
+		createItem.setAlignmentX(CENTER_ALIGNMENT);
+		itemPanel.add(createItem);
+		
+		itemPanel.add(Box.createRigidArea(new Dimension(0,100)));
+
+		deleteItem = new JButton("Delete Item");
+		deleteItem.setMaximumSize(centerPaneButtonDimension);
+		deleteItem.setAlignmentX(CENTER_ALIGNMENT);
+		itemPanel.add(deleteItem);
+		
+		recipePanel = new JPanel();
+		recipePanel.setPreferredSize(new Dimension((dim.width/4)*3,dim.height));
+		recipePanel.setLayout(new BoxLayout(recipePanel,BoxLayout.Y_AXIS));
+		
+		JLabel recipeHead = new JLabel("Recipe Menu");
+		recipeHead.setAlignmentX(CENTER_ALIGNMENT);
+		recipePanel.add(recipeHead);
+		
+		recipePanel.add(Box.createRigidArea(new Dimension(0,100)));
+		
+		JButton addRecipe =  new JButton("Add Recipe");
+		addRecipe.setMaximumSize(centerPaneButtonDimension);
+		addRecipe.setAlignmentX(CENTER_ALIGNMENT);
+		recipePanel.add(addRecipe);
+		
+		recipePanel.add(Box.createRigidArea(new Dimension(0,100)));
+		
+		JButton suggestRecipe = new JButton("Suggest Recipe");
+		suggestRecipe.setMaximumSize(centerPaneButtonDimension);
+		suggestRecipe.setAlignmentX(CENTER_ALIGNMENT);
+		recipePanel.add(suggestRecipe);
 		
 		
-		add(centerPane,BorderLayout.CENTER);
 		setVisible(true);
 	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if( e.getSource() == manageFridge ) {
+			remove(itemPanel);
+			remove(recipePanel);
+			manageFridgePane.setVisible(true);
+			add(manageFridgePane,BorderLayout.CENTER);
+			revalidate();
+			repaint();
+		}
+		else if( e.getSource() == manageItems ) {
+			remove(manageFridgePane);
+			remove(recipePanel);
+			itemPanel.setVisible(true);
+			add(itemPanel, BorderLayout.CENTER);
+			revalidate();
+			repaint();
+		}
+		else if( e.getSource() == recipes ) {
+			remove(itemPanel);
+			remove(manageFridgePane);
+			recipePanel.setVisible(true);
+			add(recipePanel, BorderLayout.CENTER);
+			revalidate();
+			repaint();
+		}
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		new Dashboard();
 	}
 
